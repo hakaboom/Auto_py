914fe9b862464cc0da4995eb221b6006511ae9d3
hakaboom <43911741+hakaboom@users.noreply.github.com>
2021-02-08 21:33:06 +0800

    README.md

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 import time
-# import re
-# import sys
+import re
+import sys
 import cv2
-# import numpy as np
+import numpy as np
 from core.cv.match_template import find_templates
 from core.run import Android
-# from loguru import logger
+from loguru import logger
 from coordinate_transform import Anchor, Point, Size, Rect
 
-# a = Android(device_id='emulator-5562', cap_method='minicap', touch_method='adbtouch')
+a = Android(device_id='emulator-5562', cap_method='minicap', touch_method='adbtouch')
