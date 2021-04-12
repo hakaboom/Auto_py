97258e025b4e7967f233643a74c17354b5815017
hakaboom <43911741+hakaboom@users.noreply.github.com>
2021-04-12 23:28:54 +0800

    修改touch的屏幕方向坐标转换

--- core/touch_method/base.py
+++ core/touch_method/base.py
@@ -5,35 +5,24 @@ from core.adb import ADB
 
 class transform(object):
     def __init__(self, adb: ADB):
-        self.size_info = adb.getPhysicalDisplayInfo()
-        # self.event_size = dict(width=display_info['max_x'], height=display_info['max_y'])
-        # self.screen_size = dict(width=display_info['width'], height=display_info['height'])
-        # self.event_scale = self.event2windows()
+        self.size_info = adb.get_display_info()
+
+    def transform(self, x, y):
+        x, y = self.ori_transform(x, y)
+        x, y = self.transform_xy(x, y)
+        return x, y
+
+    def ori_transform(self, x, y):
+        w, h = self.size_info['width'], self.size_info['height']
+        if self.size_info['orientation'] == 0:
+            x, y = x, y
+        elif self.size_info['orientation'] == 1:
+            x, y = w - y, x
+        elif self.size_info['orientation'] == 2:
+            x, y = w - x, h - y
+        elif self.size_info['orientation'] == 3:
+            x, y = y, h - x
+        return x, y
 
     def transform_xy(self, x, y):
         return x, y
-
-    # def event2windows(self):
-    #     return {
-    #         'width': self.screen_size['width'] / self.event_size['width'],
-    #         'height': self.screen_size['height'] / self.event_size['height']
-    #     }
-    #
-    # def transform(self, x, y):
-    #     if self.display_info['orientation'] == 0:
-    #         return self.right2right(x, y)
-    #     elif self.display_info['orientation'] == 1:
-    #         return self.left2right(x, y)
-    #     elif self.display_info['orientation'] == 2:
-    #         return self.portrait2right(x, y)
-    #
-    # def right2right(self, x, y):
-    #     return round(x / self.event_scale['width']), round(y / self.event_scale['width'])
-    #
-    # def portrait2right(self, x, y):
-    #     return round((x / self.screen_size['height'] * self.screen_size['width']) / self.event_scale['width']), \
-    #            round((y / self.screen_size['width'] * self.screen_size['height'] / self.event_scale['height']))
-    #
-    # def left2right(self, x, y):
-    #     return round((1 - x / self.screen_size['height']) * self.screen_size['width'] / self.event_scale['height']), \
-    #            round((1 - y / self.screen_size['width']) * self.screen_size['height'] / self.event_scale['height'])
